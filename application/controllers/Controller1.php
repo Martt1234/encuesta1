7a4d482f9c733d1d95687d131ce85383a0f37e92
Mullensini <andres-2609@outlook.com>
2019-06-23 17:42:48 -0500

    Create Controller1.php

---
+++ application/controllers/Controller1.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Controller1 extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('vistas/headers');
+		$this->load->view('vistas/vgeneral');
+	}
+
+function preguntas(){
+	$this->load->view('vistas/headers');
+$this->load->view('vistas/vpreguntas');
+}//termina la funcion preguntas
+
+}//termina la clase controller1
