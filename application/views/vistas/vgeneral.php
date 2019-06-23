aa1642486e74eaa67ee1e085ace455387ee4ed4e
Mullensini <andres-2609@outlook.com>
2019-06-23 17:48:57 -0500

    Create vgeneral.php

---
+++ application/views/vistas/vgeneral.php
@@ -0,0 +1,19 @@
+<div class="row">
+  <div class= "col-md-4"></div>
+  <div class= "col-md-4">
+    <div class="well"><!--sombreado-->
+      <center>
+        <br><br>
+        <div id= "titulo"><h1>Sistema Encuestas</h1></div>
+           <br><br>
+           <div>
+          <a   class="btn btn-info" href="<?php echo site_url('Controller1/preguntas');?>" role = "button">Preguntas</a> 
+
+           <a   class="btn btn-warning" href="<?php echo site_url('');?>" role = "button">Cuestionario</a>
+           </div>            
+         </center>
+    </div>
+  </div>
+</div>
+</body>
+</html>
