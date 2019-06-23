065a2478435c709dfe31f4281f1bf4c02a7a6a95
Mullensini <andres-2609@outlook.com>
2019-06-23 18:25:27 -0500

    Update vactpregunta.php

--- application/views/vistas/vactpregunta.php
+++ application/views/vistas/vactpregunta.php
@@ -1,3 +1,13 @@
-<div id="titutlo1"> <h1>Modificar Pregunta</h1></div>
+<div class="row">
+  <div class= "col-md-4"></div>
+  <div class= "col-md-4">
+    <div class="well"><!--sombreado-->
+      <center>
+        <br><br>
+        <div id= "titulo2"><h1>Modificar Pregunta</h1></div>            
+         </center>
+    </div>
+  </div>
+</div>
 </body>
 </html>
