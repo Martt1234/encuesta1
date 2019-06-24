3cbd299d0bac051b732b63d77f0f06741a50bd5c
Mullensini <andres-2609@outlook.com>
2019-06-24 12:37:01 -0500

    Update velimpreguntas.php

--- application/views/vistas/velimpreguntas.php
+++ application/views/vistas/velimpreguntas.php
@@ -1,9 +1,23 @@
-<div>
+<div class= "row">
  <div class="col-md-4"></div>
  <div class= "colmd-4"><br><br>
+ 	<center>
  	<div id = "titulo3"><h1>Eliminar Preguntas</h1></div>
- 	
- </div>	
+    <br><br>
+
+ 	<table class="table">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">Descripción</th>
+      <th scope="col">Acción</th>
+    </tr>
+  </thead>
+  <tbody>
+  </tbody>
+    </table>
+  </center>
+</div>	
 </div>
 </body>
 </html>
