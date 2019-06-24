b720e9a020b1582b581ab1464039ac0d5d9283e0
Mullensini <andres-2609@outlook.com>
2019-06-24 17:59:29 -0500

    Update vgeneral.php

--- application/views/vistas/vgeneral.php
+++ application/views/vistas/vgeneral.php
@@ -7,7 +7,7 @@
         <div id= "titulo"><h1>Sistema Encuestas</h1></div>
            <br><br>
            <div>
-          <a   class="btn btn-info" href="<?php echo site_url('Controller1/preguntas');?>" role = "button">Preguntas</a> 
+          <a   class="btn btn-info" href="<?php echo site_url('Controller1/preguntas');?>" role = "button">Preguntas alta</a> 
 
            <a   class="btn btn-warning" href="<?php echo site_url('');?>" role = "button">Cuestionario</a>
            </div>            
