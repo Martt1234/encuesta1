e14f6a262e39532ddb9e3876684dac056bdaa91a
Mullensini <andres-2609@outlook.com>
2019-06-24 12:20:55 -0500

    Update velimpreguntas.php

--- application/views/vistas/velimpreguntas.php
+++ application/views/vistas/velimpreguntas.php
@@ -1,9 +1,8 @@
 <div>
  <div class="col-md-4"></div>
- <div class= "colmd-4">
- 	<h1>Ola mundo nenes</h1>
-
-
+ <div class= "colmd-4"><br><br>
+ 	<div id = "titulo3"><h1>Eliminar Preguntas</h1></div>
+ 	
  </div>	
 </div>
 </body>
