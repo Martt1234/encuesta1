e61a6aeb8e069d4cbf803d0c1f933557f07095d5
Mullensini <andres-2609@outlook.com>
2019-06-24 17:51:09 -0500

    Update vpreguntas.php

--- application/views/vistas/vpreguntas.php
+++ application/views/vistas/vpreguntas.php
@@ -1,20 +1,26 @@
 <div class="row">
-	<div class= "col-md-4"></div>
-	<div class= "col-md-4">
-		<div class="well"><!--sombreado-->
-			<center>
-				<br><br>
-				<div id= "titulo1"><h1>Preguntas</h1></div>
-				   <br><br>
+  <div class= "col-md-4"></div>
+  <div class= "col-md-4">
+    <div class="well"><!--sombreado-->
+      <center>
+        <br><br>
+        <div id= "titulo1"><h1>Preguntas</h1></div>
+           <br><br>
                     <div class="container">
-     <br><br>
+     
        <!-- Button trigger modal -->
       <button type="button"  class="btn btn-outline-info"data-toggle="modal" data-target="#exampleModal">
         Agregar
       </button>
-      	<a   class="btn btn-outline-success" href="<?php echo site_url('');?>" role = "button">Modificar</a> 
-           <a  class="btn btn-outline-danger" href="<?php echo site_url('');?>" role = "button">Eliminar</a>
 
+        <a   class="btn btn-outline-secondary" href="<?php echo site_url('Controller1/actualizarpreg');?>" role = "button">Actualizar</a>
+
+      <a   class="btn btn-outline-danger" href="<?php echo site_url('Controller1/borrarpreg');?>" role = "button">Eliminar</a>
+           <br><br>
+           <!--enlace que te regresa a la pagina de principal-->
+           <a href="<?php echo site_url('Controller1/index');?>" style="color:#154360"><strong><u>Volver al inicio</u></strong></a>
+           <br><br>
+       
 <!-- Modal -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -27,10 +33,10 @@
       </div>
       <div class="modal-body">
         <!--con site url creas el enlace al controlador-->
-         <form method="post" action="<?php echo site_url('')?>">
+         <form method="post" action="<?php echo site_url('Controller1/altaPregunta')?>">
     <div class="form-group">
     <label for="exampleInputEmail1">Pregunta</label>
-    <input type="Text" class="form-control" name = "" aria-describedby="emailHelp" placeholder="Escribe pregunta">
+    <input type="Text" class="form-control" name = "descripcion" aria-describedby="emailHelp" placeholder="Escribe tu pregunta">
     </div>
    <br>
     <button type="submit"  class="btn btn-outline-info" value="Agregar">Agregar</button>
@@ -42,11 +48,11 @@
     </div>
   </div>
 </div>
- 
+
      </div>                             
          </center>
-		</div>
-	</div>
+    </div>
+  </div>
 </div>
 </body>
 </html>
