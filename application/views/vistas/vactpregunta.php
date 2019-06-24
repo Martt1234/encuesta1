4caca64d10e364e677d2df440376287d86e4dfdc
Mullensini <andres-2609@outlook.com>
2019-06-24 12:36:55 -0500

    Update vactpregunta.php

--- application/views/vistas/vactpregunta.php
+++ application/views/vistas/vactpregunta.php
@@ -4,7 +4,22 @@
     <div class="well"><!--sombreado-->
       <center>
         <br><br>
-        <div id= "titulo2"><h1>Modificar Pregunta</h1></div>            
+        <div id= "titulo2"><h1>Modificar Pregunta</h1></div>
+        <br>
+         <table class="table">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">Descripción</th>
+      <th scope="col">Acción</th>
+    </tr>
+  </thead>
+      <tbody>
+       </tbody>
+   </table>
+
+
+
          </center>
     </div>
   </div>
