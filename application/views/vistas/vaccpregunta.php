8c7ac136a2d060bb0a60d54cc7189ede36318596
Mullensini <andres-2609@outlook.com>
2019-06-30 22:16:44 -0500

    Create vaccpregunta.php

---
+++ application/views/vistas/vaccpregunta.php
@@ -0,0 +1,28 @@
+<center>	
+<br><br>
+<div id= "titulo3"><h1>Accion Pregunta</h1></div>
+<br><br>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col"># id</th>
+      <th scope="col">Descripcion de la pregunta</th>
+      <th scope="col">Accion-Pregunta</th>
+    </tr>
+  </thead>
+  <tbody>
+ <?php foreach($result as $row) {?>
+                <tr>
+                <th scope="row"><?php echo $row->id; ?></th>
+                <td><?php echo $row->descripcion; ?></td>
+              <td> <a  class= "btn btn-outline-success"href="<?php echo site_url('Controller1/editarpreg');?>/<?php echo $row->id;?>" role = "button">Editar</a> 
+                  <td><a class="btn btn-outline-danger" href= "<?php echo site_url('Controller1/eliminar'); ?>/<?php echo $row->id;?>"role="button">Borrar</a>
+                <?php } ?>    
+      
+  </tbody>
+</table>
+
+ <a href="<?php echo site_url('Controller1/preguntas');?>" style="color:#154360"><strong><u>Volver a la pagina anterior</u></strong></a>
+</center>
+</body>
+</html>
