a323d8f56f465280e04172bd3ed60b20985b75f1
Mullensini <andres-2609@outlook.com>
2019-06-30 22:33:15 -0500

    Update vgeneral.php

--- application/views/vistas/vgeneral.php
+++ application/views/vistas/vgeneral.php
@@ -1,19 +1,14 @@
-<div class="row">
-  <div class= "col-md-4"></div>
-  <div class= "col-md-4">
-    <div class="well"><!--sombreado-->
-      <center>
-        <br><br>
-        <div id= "titulo"><h1>Sistema Encuestas</h1></div>
-           <br><br>
-           <div>
-          <a   class="btn btn-info" href="<?php echo site_url('Controller1/preguntas');?>" role = "button">Preguntas</a> 
+<br>
+<div id= "titulo" align="center"><h1>Sistema Encuestas</h1></div>  
+ <br><br>
+  <div  style="float:left;margin-left:250px;">
+    <a   class="btn btn-outline-info" href="<?php echo site_url('Controller1/preguntas');?>">
+    <img src="<?php echo base_url('assets/img/preguntas.jpg');?> ">Preguntas</a></div>
 
-           <a   class="btn btn-warning" href="<?php echo site_url('Controller1/cuestionario');?>" role = "button">Cuestionario</a>
-           </div>            
-         </center>
-    </div>
+  <div  style="float:right;margin-right:250px;" >
+    <a  class="btn btn-outline-warning" href="<?php echo site_url('Controller1/cuestionario');?>"><img src="<?php echo base_url('assets/img/Cuestionario1.jpg');?>">cuestionario</a>
   </div>
-</div>
+
+              
 </body>
 </html>
